3f4a5d3d793d7a4ecd60896106837ef44ef0aa4b
Thor Garcia <thor.fc21@gmail.com>
2018-08-20 12:57:39 -0300

    project: update arrow functions to function
    update arrow functions ocurrences in project to 'function' because  we are
    Old$chool baby

--- test/e2e/index.js
+++ test/e2e/index.js
@@ -1,8 +1,8 @@
 var test = require('ava')
 var cep = require('../../src')
 
-test.cb('should return correct address with "05010000" string', t => {
-  cep('05010000', (err, cepData) => {
+test.cb('should return correct address with "05010000" string', function (t) {
+  cep('05010000', function (err, cepData) {
     t.is(err, null)
 
     t.deepEqual(cepData, {
@@ -17,8 +17,8 @@ test.cb('should return correct address with "05010000" string', t => {
   })
 })
 
-test.cb('should return correct address with a valid 5010000 number', t => {
-  cep(5010000, (err, cepData) => {
+test.cb('should return correct address with a valid 5010000 number', function (t) {
+  cep(5010000, function (err, cepData) {
     t.is(err, null)
 
     t.deepEqual(cepData, {
@@ -33,8 +33,8 @@ test.cb('should return correct address with a valid 5010000 number', t => {
   })
 })
 
-test.cb('should return error with an inexistent "99999999" CEP', t => {
-  cep('99999999', (err, cepData) => {
+test.cb('should return error with an inexistent "99999999" CEP', function (t) {
+  cep('99999999', function (err, cepData) {
     t.is(err.name, 'CepPromiseError')
     t.is(err.message, 'Todos os serviços de CEP retornaram erro.')
     t.is(err.type, 'service_error')
@@ -45,8 +45,8 @@ test.cb('should return error with an inexistent "99999999" CEP', t => {
   })
 })
 
-test.cb('should return validation error when invoked with an invalid "123456789" CEP', t => {
-  cep('123456789', (err, cepData) => {
+test.cb('should return validation error when invoked with an invalid "123456789" CEP', function (t) {
+  cep('123456789', function (err, cepData) {
     t.is(err.name, 'CepPromiseError')
     t.is(err.message, 'CEP deve conter exatamente 8 caracteres.')
     t.is(err.type, 'validation_error')
@@ -56,4 +56,3 @@ test.cb('should return validation error when invoked with an invalid "123456789"
     t.end()
   })
 })
-
